e4c54e584927dc98c0b433fde0e9f3fbf8610cb8
Richard Pon <rpon@box.com>
2015-04-10 22:19:10 -0700

    Remove items from the list using the LongClickListener

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -30,6 +31,8 @@ public class MainActivity extends ActionBarActivity {
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+
+        setupListViewListener();
     }
 
 
@@ -62,4 +65,21 @@ public class MainActivity extends ActionBarActivity {
         itemsAdapter.add(itemText);
         etNewItem.setText("");
     }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+            new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> adapter,
+                        View view, int pos, long id) {
+                    items.remove(pos);
+                    itemsAdapter.notifyDataSetChanged();
+                    return true;
+                }
+        });
+    }
+
+
+
+
 }
